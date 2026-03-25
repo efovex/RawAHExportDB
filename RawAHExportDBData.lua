21708bf6fa71f0ef2a05f96031eb16cef1852fde
unknown <chrisver@hotmail.com>
2026-03-25 22:30:16 +0000

    ExportData

---
+++ RawAHExportDBData.lua
@@ -0,0 +1,52 @@
+
+RAW_AH_EXPORT_DB = {
+["meta"] = {
+["lastTrackedItemCount"] = 3,
+["lastScanAt"] = 1774477307,
+["lastFilteredRowCount"] = 0,
+["lastScanRealm"] = "Thunderstrike-Alliance",
+["lastItemSummaryCount"] = 1,
+},
+["version"] = 1,
+["scans"] = {
+{
+["realmKey"] = "Thunderstrike-Alliance",
+["trackedItemCount"] = 3,
+["topUntracked"] = {
+{
+["isTracked"] = false,
+["totalQuantity"] = 919394,
+["itemID"] = 0,
+["seenWithBuyoutCount"] = 195351,
+["maxBuyout"] = 999999999,
+["minBuyout"] = 2,
+["auctionCount"] = 196409,
+["name"] = "",
+},
+},
+["realmName"] = "Thunderstrike",
+["filteredRows"] = {
+},
+["itemSummaryCount"] = 1,
+["scannedAtEpoch"] = 1774477307,
+["scannedAtText"] = "2026-03-25T22:21:47Z",
+["player"] = "Allybank",
+["faction"] = "Alliance",
+["filteredRowCount"] = 0,
+["completed"] = true,
+["requestedAt"] = 1774477241,
+["itemSummary"] = {
+[0] = {
+["isTracked"] = false,
+["totalQuantity"] = 919394,
+["itemID"] = 0,
+["seenWithBuyoutCount"] = 195351,
+["maxBuyout"] = 999999999,
+["minBuyout"] = 2,
+["auctionCount"] = 196409,
+["name"] = "",
+},
+},
+},
+},
+}
